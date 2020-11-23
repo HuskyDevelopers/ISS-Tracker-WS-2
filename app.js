e5a9eaa41935cb8e48353dbc2d3c5402d9252c10
Allan Lian <allanlian@MacBook-Pro.local>
2020-11-22 19:50:02 -0500

    update

--- app.js
+++ app.js
@@ -15,3 +15,35 @@ var myCircle = {
 
 const marker = L.marker([0, 0], { icon: myIcon }).addTo(mymap);
 const circle = L.circle([0, 0], myCircle).addTo(mymap);
+
+///////////////////////////////////////////////////////////////////
+
+const api_url = 'https://api.wheretheiss.at/v1/satellites/25544';
+
+const latitude_element = document.getElementById('iss_Lat');
+const longitude_element = document.getElementById('iss_Long');
+
+let initialCall = true;
+
+async function getISSLocation() {
+    const response = await fetch(api_url);
+    const data = await response.json();
+
+    const iss_Lat = data.latitude;
+    const iss_Long = data.longitude;
+
+    latitude_element.innerText = "Latitude: " + iss_Lat.toFixed(6);
+    longitude_element.innerText = "Longitude: " + iss_Long.toFixed(6);
+
+    marker.setLatLng([iss_Lat, iss_Long]);
+    circle.setLatLng([iss_Lat, iss_Long]);
+
+    if (initialCall) {
+        mymap.setView([iss_Lat, iss_Long], 2);
+        initialCall = false;
+    }
+}
+
+getISSLocation();
+
+setInterval(getISSLocation, 1000);
