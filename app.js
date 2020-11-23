387493e8ba0137fb418f5f560671642d77dde372
Rahul Bhagwani <bhagwani@Rahuls-MacBook-Pro.local>
2020-11-22 19:51:04 -0500

    final

--- app.js
+++ app.js
@@ -15,3 +15,25 @@ var myCircle = {
 
 const marker = L.marker([0, 0], { icon: myIcon }).addTo(mymap);
 const circle = L.circle([0, 0], myCircle).addTo(mymap);
+
+//////////////////////////////////////////////////////
+const api_url = 'https://api.wheretheiss.at/v1/satellites/25544'
+
+const latitude_element = document.getElementById('iss_Lat');
+const longitude_element = document.getElementById('iss_Long');
+
+//need async when using apis//
+async function getISSLocation() {
+    const response = await fetch(api_url);//Waiting for the api to fetch//
+    const data = await response.json(); //converting to json object//
+
+    const iss_Lat = data.latitude;
+    const iss_Long = data.longitude;
+
+    latitude_element.innerText = "Current Latitude: " + iss_Lat.toFixed(6);
+    longitude_element.innerText = "Current Longitude: " + iss_Long.toFixed(6);
+}
+
+getISSLocation();
+
+setInterval(getISSLocation, 1000); //Repeatedly calls function getISSLocation
