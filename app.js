1075ce57607a0b6abb4179cdf95e6180b4b8fe3c
Rashana Weerasinghe <Rashana@MacBook-Pro-2.local>
2020-11-22 20:17:03 -0500

    changes

--- app.js
+++ app.js
@@ -15,3 +15,25 @@ var myCircle = {
 
 const marker = L.marker([0, 0], { icon: myIcon }).addTo(mymap);
 const circle = L.circle([0, 0], myCircle).addTo(mymap);
+
+
+
+const api_url = 'https://api.wheretheiss.at/v1/satellites/25544';
+
+const latitude_element = document.getElementById('iss_Lat');
+const longitude_element = document.getElementById('iss_Long');
+
+asyncfunction getISSLocation() {
+    const response = await fetch(api_url);
+    const data = await response.json();
+
+    const iss_Lat = data.latitude;
+    const iss_Long = data.longitude;
+
+    latitude_element.innerText = "Latitude:" + iss_lat.toFixed(6);
+    longitude_element.innerText = "Longitude:" + iss_lat.toFixed(6);
+}
+
+getISSLocation();
+
+setInterval(getISSLocation, 1000);
