d461027400fa2e8031ff87f489c135dc74242a11
Aditya Dubey <aditya125.ad@gmail.com>
2020-11-22 19:53:12 -0500

    final app

--- app.js
+++ app.js
@@ -1,7 +1,14 @@
-var mymap = L.map('mapid').setView([0, 0], 2);
+const mymap = L.map('mapid').setView([0, 0], 2);
+const attribution =
+    '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors';
+const tileUrl = 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png';
+const tiles = L.tileLayer(tileUrl, { attribution });
+
+tiles.addTo(mymap);
 
 var myIcon = L.icon({
     iconUrl: 'issPic.png',
+
     iconSize: [75, 48],
     iconAnchor: [37.5, 22.5],
 });
@@ -28,16 +35,13 @@ let initialCall = true;
 async function getISSLocation() {
     const response = await fetch(api_url);
     const data = await response.json();
-
     const iss_Lat = data.latitude;
     const iss_Long = data.longitude;
-
-    latitude_element.innerText = 'Latitude: ' + iss_Lat.toFixed(6);
-    longitude_element.innerText = 'Longitude: ' + iss_Long.toFixed(6);
+    latitude_element.innerText = iss_Lat.toFixed(6);
+    longitude_element.innerText = iss_Long.toFixed(6);
 
     marker.setLatLng([iss_Lat, iss_Long]);
     circle.setLatLng([iss_Lat, iss_Long]);
-
     if (initialCall) {
         mymap.setView([iss_Lat, iss_Long], 2);
         initialCall = false;
